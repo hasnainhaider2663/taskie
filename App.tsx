445b53d75111830f59b74917590302ae272951d5
muhammadhasnainhaider <hasnainhaider2663@gmail.com>
2023-04-27 01:03:41 +0500

    h

--- App.tsx
+++ App.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import auth from '@react-native-firebase/auth';
+import firebase from '@react-native-firebase/app';
+
 import { GoogleSignin } from 'react-native-google-signin';
 import LoginScreen from './src/screens/login';
 
@@ -12,12 +13,21 @@ type RootStackParamList = {
 };
 
 const Stack = createStackNavigator<RootStackParamList>();
-
+const firebaseConfig = {
+  apiKey: "AIzaSyBX9m6I7IXCci3CMHdb3WAFnDRvK10a0lI",
+  authDomain: "taskie-38162.firebaseapp.com",
+  projectId: "taskie-38162",
+  storageBucket: "taskie-38162.appspot.com",
+  messagingSenderId: "568178372932",
+  appId: "1:568178372932:web:9e1fa63ce61e154c5b2711",
+  measurementId: "G-H7YDQWSVJR"
+};
 const App = () => {
   // Initialize Firebase
-  if (!auth().app.options.appId) {
-    auth().app.initializeApp();
+  if (!firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig);
   }
+  
 
   // Configure Google Sign-In
   GoogleSignin.configure({
